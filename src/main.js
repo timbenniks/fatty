088f1911651c80eb1ccf2c1f93c7c257341c9978
Tim Benniks <tbenniks@gmail.com>
2019-12-07 00:03:02 +0100

    added google analytics

--- src/main.js
+++ src/main.js
@@ -3,10 +3,16 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
+import VueAnalytics from "vue-analytics";
 
 Vue.config.productionTip = false;
 
 import "@/styles/index.scss";
+
+Vue.use(VueAnalytics, {
+  id: "UA-6797812-7",
+  router
+});
 
 new Vue({
   router,
